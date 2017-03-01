bb050649aa55c9bf1712cf4430938bda41e2a3ff
kicohen <kicohen@andrew.cmu.edu>
2017-03-01 13:15:10 -0500

    Changed initial settings and created models.

--- microsoftspace/settings.py
+++ microsoftspace/settings.py
@@ -119,3 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login'
+
+MAX_UPLOAD_SIZE = "5242880"
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
