6fa474e8b5b839678c5edd5aa2168ee0543a7603
kicohen <kicohen@andrew.cmu.edu>
2017-03-01 16:59:59 -0500

    Added initial user views and actions. About to add email functionality.

--- msevents/urls.py
+++ msevents/urls.py
@@ -4,7 +4,10 @@ from msevents import views as msevents_views
 
 urlpatterns = [
     url(r'^$', msevents_views.home, name='home'),
-    url(r'^login$', auth_views.login, {'template_name':'socialnetwork/login.html'}, name='login'),
-    url(r'^logout/$',auth_views.logout, {'next_page':'login'}, name='logout'),
+    url(r'^$', msevents_views.profile, name='profile'),
+    url(r'^login$', auth_views.login, {'template_name':'msevents/login.html'}, name='login'),
+    url(r'^logout/$',auth_views.logout, {'next_page':'home'}, name='logout'),
+    url(r'^register$', msevents_views.register, name='register'),
+    url(r'^confirm-registration/(?P<username>[a-zA-Z0-9_@\+\-]+)/(?P<token>[a-z0-9\-]+)$',
+        msevents_views.confirm_registration, name='confirm'),
 ]
-
