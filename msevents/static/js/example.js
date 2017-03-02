e8f0e8e8297649f0261e416c9488781c6a39cbad
kicohen <kicohen@andrew.cmu.edu>
2017-03-01 20:19:41 -0500

    Calendar takes data from database and added favicon.

--- msevents/static/js/example.js
+++ msevents/static/js/example.js
@@ -1,44 +1,11 @@
-angular.module('mwl.calendar.docs', ['mwl.calendar', 'ngAnimate', 'ui.bootstrap', 'colorpicker.module']);
-angular
-  .module('mwl.calendar.docs') //you will need to declare your module with the dependencies ['mwl.calendar', 'ui.bootstrap', 'ngAnimate']
-  .controller('KitchenSinkCtrl', function(moment, alert, calendarConfig) {
-
+angular.module('mwl.calendar.docs', ['mwl.calendar', 'ngAnimate', 'ui.bootstrap', 'colorpicker.module']).controller('KitchenSinkCtrl', function(moment, alert, calendarConfig) {
     var vm = this;
-
     //These variables MUST be set as a minimum for the calendar to work
     vm.calendarView = 'month';
     vm.viewDate = new Date();
     var actions = [{}];
-    vm.events = [
-      {
-        title: 'An event',
-        color: calendarConfig.colorTypes.warning,
-        startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
-        endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
-        draggable: true,
-        resizable: true,
-        actions: actions
-      }, {
-        title: '<span class="text-primary">Another event</span>, with a <i>html</i> title',
-        color: calendarConfig.colorTypes.info,
-        startsAt: moment().subtract(1, 'day').toDate(),
-        endsAt: moment().add(5, 'days').toDate(),
-        draggable: true,
-        resizable: true,
-        actions: actions
-      }, {
-        title: 'This is a really long event title that occurs on every year',
-        color: calendarConfig.colorTypes.important,
-        startsAt: moment().startOf('day').add(7, 'hours').toDate(),
-        endsAt: moment().startOf('day').add(19, 'hours').toDate(),
-        recursOn: 'year',
-        draggable: true,
-        resizable: true,
-        actions: actions
-      }
-    ];
-
-    vm.cellIsOpen = true;
+    
+    vm.cellIsOpen = false;
 
     vm.addEvent = function() {
       vm.events.push({
@@ -46,26 +13,25 @@ angular
         startsAt: moment().startOf('day').toDate(),
         endsAt: moment().endOf('day').toDate(),
         color: calendarConfig.colorTypes.important,
-        draggable: true,
-        resizable: true
+        draggable: false,
+        resizable: false
       });
     };
 
     vm.eventClicked = function(event) {
       console.log('event_clicked')
-      alert.show('Clicked', event);
     };
 
     vm.eventEdited = function(event) {
-      alert.show('Edited', event);
+      console.log('event edited')
     };
 
     vm.eventDeleted = function(event) {
-      alert.show('Deleted', event);
+      console.log('event deleted')
     };
 
     vm.eventTimesChanged = function(event) {
-      alert.show('Dropped or resized', event);
+      console.log('event times changed')
     };
 
     vm.toggle = function($event, field, event) {
